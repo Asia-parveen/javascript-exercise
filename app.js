809c25f939eb4128fc245adcaa0e3ddb613b08c0
Asia-parveen <beautyhealthytips@gmail.com>
2024-04-30 05:54:49 -0700

    added files

---
+++ app.js
@@ -0,0 +1,37 @@
+// chapter-2
+
+// q1_ decleration veriable called userName
+// let uaerName;
+
+//q2_
+// let myName = "Asia Parveen";
+
+//q3_
+// let message = "Hello world";
+// alert( message);
+
+//q4_
+// let stdData = "sadia";
+// stdAge = "15 years old ";
+// stdStatus = "CERTIFIED MOBILE APPLICATION DEVLOPMINT";
+
+//  alert(stdData);
+//  alert(stdAge);
+//  alert(stdStatus);
+
+//q5_
+// let food = "Pizza \n Pizz \n Piz \n Pi \n p";
+// alert(food);
+
+// q6_
+// let email = "comtech@gmail.com";
+// alert(`My email address is ${email}`);
+
+//q7_
+// let book = "A smater way to Learn Javascript";
+// alert(`I am traying to learn from the Book ${book}`);
+
+
+
+
+
