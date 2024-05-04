87be7179974ddba13d26b0cce1e9f4c443eaaefe
Asia-parveen <beautyhealthytips@gmail.com>
2024-05-04 11:27:55 -0700

    add more work

--- app.js
+++ app.js
@@ -31,6 +31,132 @@
 // let book = "A smater way to Learn Javascript";
 // alert(`I am traying to learn from the Book ${book}`);
 
+//8. Write a script to display this in browser through JS.
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// 9. Store following string in a variable and show in alert and
+// browser through JS
+// “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”
+
+// let imgclue = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(imgclue);
+// document.write(imgclue);
+
+// chapter 2 done.
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+// chapter 3 start.
+// VARIABLES FOR NUMBERS
+
+// 1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+
+// let age = 15;
+// alert(`I am ${age} years old`);
+
+// 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+
+// let vesCount = 14;
+// alert(`You have visited this site ${vesCount} times`);
+
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+
+//  let myYear = 1990;
+// document.write(`My birth year is ${ myYear}<br/> Data type of my decleared variable is number`);
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+// let a = "John Doe";
+// let b = "5 T-Shirts(s)";
+// let c = "XYZ";
+// console.log(`"${a} orderes ${b} on ${c} Clothing store".`);
+// document.write(`" <b>${a}</b> orderes <b>${b}</b> on <b>${c}</b> Clothing store".`);
+
+// chapter 3 ended
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+// chapter 4 started
+// VARIABLE NAMES: LEGAL & ILLEGAL
+
+// 1. Declare 3 variables in one statement.
+// 2. Declare 5 legal & 5 illegal variable names.
+// 3. Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+
+// 1.
+// let myName = "Asia";
+// let qualification = "B.sc";
+// let student = "Smit";
+// console.log(`"Hi everyone i am ${myName} and i have done ${qualification} Now i am a student of  ${ student} "`);
+// document.write(`"Hi everyone i am <b> ${myName}</b> and i have done <b> ${qualification}</b> Now i am a student of <b> ${ student} </b>."`);
+
+//2. Declare 5 legal variable names.
+// let myName = "Asia";
+// var MyName = "Asia";
+// let $myname = "Asia";
+// let my_name = "Asia";
+// var myname2 = "Asia";
+
+//2. Declare 5 illlegal variable names.
+
+//  let 0firstnumber = 20;
+//  var #number = 20;
+//  let my  number = 20;
+//  var &name = 20;
+//  let --number = 20;
+
+// 3. Display this in your browser.
+//a.
+// let message = "Rules for naming JS variables";
+// document.write(`<b>${message}</b>`);
+// console.log(`${message}`);
+
+//b.
+// let a = ",";
+// let b = "numbers";
+// let c = "$";
+// let d = "_";
+// document.write(`variable names can only contain ${b}, ${c} and ${d}. For example $my_1stVariable`);
+// console.log(`variable names can only contain  ${b}, ${c} and ${d}. For example $my_1stVariable`);
+
+//c.
+
+// var a = "$";
+// var b = "_";
+// document.write(`variable must begin with the latter of ${a} or ${b}.For example $name, _name or name`);
+// console.log(`variable must begin with the latter of ${a} or ${b}.For example $name, _name or name`);
+
+//d.
+  
+// var condition = "Sensitive";
+// document.write(`Variable names are case ${condition}`);
+// console.log(`Variable names are case ${condition}`);
+
+//E.
+
+// var words = "Keywords";
+// document.write(`Variable names should not be JS ${words}`);
+// console.log(`Variable names should not be JS ${words}`);
+
+// chapter 4 ended
 
 
 
